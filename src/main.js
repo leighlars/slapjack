36f2bb4423a7f7bd7c90c040b0a74ad3730fe38e
Leigh Larson <leighlarson4@gmail.com>
2020-06-05 10:41:08 -0600

    Begin logic for keyhandler(), add DOM function shells for main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,31 @@
 var game = new Game();
 
+document.addEventListener("keydown", keyHandler);
 
+function keyHandler() {
+  if (event.key === 81) {
+    game.player1.playCard();
+  }
+  if (event.key === 70) {
+    game.player1.slapPile();
+  }
+  if (event.key === 80) {
+    game.player2.playCard();
+  }
+  if (event.key === 74 ) {
+    game.player2.slapPile();
+  }
+}
 
 
 game.shuffleDeck(game.deck);
 
 game.deal();
 
-// updatePlayerWinsText() {
+//function displayTopCard() {
+//
+// }
+
+//function updatePlayerWinsText() {
 //
 // }
