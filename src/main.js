9ba5d3de1ea39b56985f74d830cf17166061c8e1
Leigh Larson <leighlarson4@gmail.com>
2020-06-06 10:42:34 -0600

    Update changeTopCard() with cards array

--- src/main.js
+++ src/main.js
@@ -23,7 +23,7 @@ function keyHandler() {
 function changeTopCard(playedCard) {
   var topCard = document.querySelector(".center-pile");
   topCard.classList.remove("hidden");
-  topCard.style.background = images.find(image => image === playedCard);
+  topCard.style.background = cards.find(card => card === playedCard);
   // still need to figure out how to connect each img with array[i]
   // player2 played the card ? topCard.classList.add("p2") : topCard.classList.remove("p2");
 }
